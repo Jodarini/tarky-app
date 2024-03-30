5fbe03366755084e8dab6c234ddc029a0c6b44fc
Jodarini <rivasjose9428@gmail.com>
2024-03-30 17:26:38 -0500

    first commit

---
+++ src/Home.tsx
@@ -0,0 +1,14 @@
+// api endpoint
+// https://api.tarkov.dev/graphql
+// items {
+//      {
+//     id
+//     name
+//   }
+// }
+export default function Home() {
+   
+  return (
+    <div>Home</div>
+  )
+}
